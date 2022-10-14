3233ea5b42774a7ea091b324b20dc651c5f8a4f8
Кирилл <samsuu@ya.ru>
2022-10-14 23:03:16 +0500

    fix webpack

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -7,7 +7,7 @@ export default function () {
   return {
     output: {
       filename: 'index.js',
-      path: path.resolve(__dirname, '../dist'),
+      path: path.resolve(__dirname, '../build'),
       publicPath: '/',
     },
     module: {
